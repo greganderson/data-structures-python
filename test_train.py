e0641624d11af956f2721df54100315cacfadf43
Greg Anderson <greg.anderson.cs@gmail.com>
2024-02-28 09:31:42 -0700

    Add implementations for most methods, including tests

--- test_train.py
+++ test_train.py
@@ -1,5 +1,3 @@
-import pytest
-
 from train import TrainCar, Train
 
 
@@ -13,6 +11,7 @@ def test_add_front():
 
     assert test_train.head_car != None
     assert test_train.head_car.contents == test_car_1_contents
+    assert test_train.size == 1
 
     # Test second car
     test_car_2_contents = "Prefects"
@@ -22,6 +21,7 @@ def test_add_front():
 
     assert test_train.head_car != None
     assert test_train.head_car.contents == test_car_2_contents
+    assert test_train.size == 2
 
 def test_add_back():
     test_train = Train()
@@ -33,6 +33,7 @@ def test_add_back():
 
     assert test_train.head_car != None
     assert test_train.head_car.contents == test_car_1_contents
+    assert test_train.size == 1
 
     # Test second car
     test_car_2_contents = "Prefects"
@@ -41,6 +42,7 @@ def test_add_back():
 
     assert test_train.head_car.next_car != None
     assert test_train.head_car.next_car.contents == test_car_2_contents
+    assert test_train.size == 2
 
 def test_insert():
     test_train = Train()
@@ -54,9 +56,51 @@ def test_insert():
     while c.next_car != None and c.contents != "Contents 5":
         c = c.next_car
     
-    new_car = TrainCar("New car")
-    test_train.insert(c, new_car)
 
+    assert test_train.size == 10
+    # head - 1 - 2 - 3 - 4 - 5
+    assert test_train.head_car.next_car.next_car.next_car.next_car.next_car.contents == "Contents 5"
+
+    c = test_train.head_car
+    while c.next_car != None:
+        c = c.next_car
+
+    new_car = TrainCar("New car")
+    # Same as add_back
+    test_train.insert(c, new_car)
+    assert c.next_car == new_car
     assert test_train.size == 11
-    assert test_train.head_car.next_car.next_car.next_car.next_car.contents == "Content 5"
-    assert test_train.head_car.next_car.next_car.next_car.next_car.contents == "New car"
+
+def test_search():
+    test_train = Train()
+
+    test_cars = [TrainCar(f"Contents {i}") for i in range(10)]
+
+    for car in test_cars:
+        test_train.add_back(car)
+
+    target_car = test_cars[5]
+
+    actual_car = test_train.search(target_car.contents)
+
+    assert target_car == actual_car
+
+def test_remove_back():
+    test_train = Train()
+
+    assert test_train.remove_back() == False
+
+    test_cars = [TrainCar(f"Contents {i}") for i in range(10)]
+    for car in test_cars:
+        test_train.add_back(car)
+
+    assert test_train.size == 10
+    assert test_train.remove_back() == True
+    assert test_train.size == 9
+
+    expected_back_car = test_cars[8]
+    actual_back_car = test_train.head_car
+    while actual_back_car.next_car != None:
+        actual_back_car = actual_back_car.next_car
+
+    assert expected_back_car == actual_back_car
--- train.py
+++ train.py
@@ -14,20 +14,52 @@ class Train:
     
     def add_front(self, car: TrainCar):
         """ Adds the train car at the front of the train """
-        pass
-    
+        if self.head_car != None:
+            car.next_car = self.head_car
+        self.head_car = car
+
+        self.size += 1
+
     def add_back(self, car: TrainCar) -> None:
         """ Adds the train car at the end of the train """
-        pass
+        if self.head_car == None:
+            self.head_car = car
+            self.size += 1
+            return
+
+        # Find last car
+        current_car = self.head_car
+        while current_car != None:
+            if current_car.next_car == None:
+                current_car.next_car = car
+                break
+            current_car = current_car.next_car
+
+        self.size += 1
 
     def insert(self, front_car: TrainCar, new_car: TrainCar) -> None:
         """ Attaches the new car to the 'front_car', replacing the car that used to be attached to 'front_car'. That will now
         be attached to the new car. """
-        pass
+        current_car = self.head_car
+
+        while current_car != None:
+            if current_car == front_car:
+                temp_car = front_car.next_car
+                new_car.next_car = temp_car
+                current_car.next_car = new_car
+                break
+            current_car = current_car.next_car
+
+        self.size += 1
 
     def search(self, contents: str) -> TrainCar:
         """ Returns the train car that has the provided contents, or None if it doesn't exist """
-        pass
+        current_car = self.head_car
+        while current_car != None:
+            if current_car.contents == contents:
+                return current_car
+            current_car = current_car.next_car
+        return None
 
     def remove_front(self) -> bool:
         """ Removes the front train car from the train """
@@ -35,7 +67,18 @@ class Train:
 
     def remove_back(self) -> bool:
         """ Removes the back train car from the train """
-        pass
+        if self.size == 0:
+            return False
+
+        current_car = self.head_car
+
+        while current_car.next_car != None:
+            if current_car.next_car.next_car == None:
+                # Found second to last car
+                current_car.next_car = None
+                self.size -= 1
+                return True
+            current_car = current_car.next_car
 
     def remove(self, car: TrainCar) -> bool:
         """ Removes the target train car from the train, attaching the car behind it to the car in front of it """
